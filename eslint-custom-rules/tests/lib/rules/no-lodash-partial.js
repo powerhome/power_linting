4f819e5098b0c177d162210a38c95f4da60bd6cd
Sergio Vilar <sergio.araujo@powerhrg.com>
2022-08-25 19:35:16 -0300

    Add more test scenarios
    # Explique porque essa mudança está sendo feita
    
    # Forneça links para tickets, artigos ou outros recursos relevantes

--- eslint-custom-rules/tests/lib/rules/no-lodash-partial.js
+++ eslint-custom-rules/tests/lib/rules/no-lodash-partial.js
@@ -1,33 +1,48 @@
-/**
- * @fileoverview Do not use Lodash&#39;s partial
- * @author
- */
 "use strict";
-
-//------------------------------------------------------------------------------
-// Requirements
-//------------------------------------------------------------------------------
 
 const rule = require("../../../lib/rules/no-lodash-partial"),
   RuleTester = require("eslint").RuleTester;
-
-
-//------------------------------------------------------------------------------
-// Tests
-//------------------------------------------------------------------------------
 
 const ruleTester = new RuleTester();
 const errorMessage = "Unexpected Lodash's partial, see: https://github.com/powerhome/rfcs/blob/main/0072-deprecate-lodash-partial.md";
 
 ruleTester.run("no-lodash-partial", rule, {
   valid: [
-    // give me some code that won't trigger a warning
+    {
+      code: "partial(foo)", // Since there's no lodash we assume it's not lodash's partial
+    },
+    {
+      code: "doSomething()",
+    },
+    {
+      code: "() => { doSomething() }",
+      parserOptions: { ecmaVersion: 6 },
+    },
+    {
+      code: "() => doSomething()",
+      parserOptions: { ecmaVersion: 6 },
+    }
   ],
-
   invalid: [
     {
-      code: "'lodash' \n partial(doSomething, 'foo')",
+      code: "import lodash from 'lodash' \n lodash.partial(doSomething, 'foo')",
       errors: [{ message: errorMessage, type: "CallExpression" }],
+      parserOptions: { ecmaVersion: 6, sourceType: "module" },
+    },
+    {
+      code: "import { partial } from 'lodash' \n partial(doSomething, 'foo')",
+      errors: [{ message: errorMessage, type: "CallExpression" }],
+      parserOptions: { ecmaVersion: 6, sourceType: "module" },
+    },
+    {
+      code: "import { partial } from 'lodash' \n partial(doSomething)",
+      errors: [{ message: errorMessage, type: "CallExpression" }],
+      parserOptions: { ecmaVersion: 6, sourceType: "module" },
+    },
+    {
+      code: "import { partial } from 'lodash' \n () => partial(doSomething)",
+      errors: [{ message: errorMessage, type: "CallExpression" }],
+      parserOptions: { ecmaVersion: 6, sourceType: "module" },
     },
   ],
 });
